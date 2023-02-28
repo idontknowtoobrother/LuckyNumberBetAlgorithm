1d8355fdb9d91b06fa121ff29c6fcf9e7d0e4372
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-03-01 05:46:28 +0700

    Create new file 'algorithmLuckyNumberBet.js' to generate and track lucky number bets

---
+++ algorithmLuckyNumberBet.js
@@ -0,0 +1,11 @@
+const hostPool = {
+    money: 90000,
+    betPool: {}, // ['56'] = [ {userId: 19, total: 2000} ]
+    generateLuckyNumber: function(){
+        for(let bet in this.betPool){
+            
+        }
+    }
+}
+
+
